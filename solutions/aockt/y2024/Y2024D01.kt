0a492f51563a834e19d537867805be6b76db93ac
Amaury Carrade <amaury@carrade.eu>
2024-12-01 20:56:40 +0100

    Part one more Kotlin-esque

--- solutions/aockt/y2024/Y2024D01.kt
+++ solutions/aockt/y2024/Y2024D01.kt
@@ -1,28 +1,28 @@
 package aockt.y2024
 
 import io.github.jadarma.aockt.core.Solution
-import org.springframework.data.util.StreamUtils
 import java.util.stream.Collectors
 import kotlin.math.abs
-import kotlin.math.absoluteValue
 
 object Y2024D01 : Solution {
 
     private fun parse(input: String): Pair<List<Int>, List<Int>> {
         return input.lines().stream()
             .map { line -> line.split(" ") }
-            .map { lineParts -> Pair(lineParts.first().toInt(), lineParts.last().toInt()) }
-            .collect(Collectors.teeing(
-                Collectors.mapping ({ p -> p.first }, Collectors.toList()),
-                Collectors.mapping ({ p -> p.second }, Collectors.toList())
-            ) { a, b -> Pair(a, b) })
+            .map { lineParts -> lineParts.first().toInt() to lineParts.last().toInt() }
+            .collect(
+                Collectors.teeing(
+                    Collectors.mapping({ p -> p.first }, Collectors.toList()),
+                    Collectors.mapping({ p -> p.second }, Collectors.toList())
+                ) { a, b -> a to b })
     }
 
     override fun partOne(input: String): Int = parse(input)
-        .let { StreamUtils.zip(it.first.sorted().stream(), it.second.sorted().stream()) { a, b -> Pair(a, b) } }
-        .map { pair -> abs(pair.first - pair.second) }
-        .reduce { a, b -> a + b }
-        .get().absoluteValue
+        .let {
+            it.first.sorted()
+                .zip(it.second.sorted())
+                .sumOf { (a, b) -> abs(a - b) }
+        }
 
     override fun partTwo(input: String): Int = parse(input).run {
         first.stream()

275d568a28124139416f975b29de5e6c84c98193
Amaury Carrade <amaury@carrade.eu>
2024-12-02 23:05:15 +0100

    Day 2

---
+++ solutions/aockt/y2024/Y2024D02.kt
@@ -0,0 +1,21 @@
+package aockt.y2024
+
+import io.github.jadarma.aockt.core.Solution
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
+object Y2024D02 : Solution {
+    private fun parse(input: String): List<List<Int>> = input.lines()
+        .map { it.split(" ").map { it.toInt() } }
+
+    private fun isSafe(report: List<Int>): Boolean {
+        val reportDiffs = report.zipWithNext { a, b -> b - a }
+        return reportDiffs.map { it.sign }.toSet().size == 1 && reportDiffs.all { it.absoluteValue in 1..3 }
+    }
+
+    override fun partOne(input: String): Int = parse(input).count { isSafe(it) }
+
+    override fun partTwo(input: String): Int = parse(input).count {
+        report -> isSafe(report) || report.indices.map { i -> isSafe(report.filterIndexed { index, _ -> index != i }) }.reduce { a, b -> a or b }
+    }
+}
